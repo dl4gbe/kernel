454a18f5ed65ffb637f9c43287c6b0f59d0e715a
GIT Admin <christoph.eisenmann@beteo,ch>
2014-04-20 11:45:11 +0200

    first test

---
+++ communication_reason.php
@@ -0,0 +1,34 @@
+<?php
+if(!defined('kernel_entry'))define('kernel_entry', true);
+require_once('include/cls_get_parameter.php');
+require_once('include/entry_point.php');
+require_once('include/application/cls_application.php');
+require_once('include/data/tables/cls_table_communication_reason.php');
+require_once('table_helpers/cls_communication_reason_helpers.php');
+$app = new cls_application();
+$get_parameter = new cls_get_parameter();
+$helper = new cls_communication_reason_helpers($app->get_db_manager(),$app);
+if (!empty($get_parameter->mode))
+{
+	$mode = strtolower($get_parameter->mode);
+	switch ($mode)
+		{
+			case "grid":
+				$helper->get_grid_page($get_parameter);
+				break;
+			case "describe":
+				break;
+			case "save":
+				break;
+			case "form":
+				break;
+			case "sql_create":
+				require_once('creator/orm/cls_orm.php');
+				$orm = new cls_orm($app->get_db_manager(),$app);
+				return $orm->create_table_sql_statement("communication_reason",$get_parameter->database_type);
+				break;
+		}
+	exit;
+}
+$helper->get_data($get_parameter);
+?>
